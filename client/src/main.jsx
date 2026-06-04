74a8d40638bb599f7fc6af65d90707eec361c4b5
Aakashah247 <aakashsah098765@gmail.com>
2026-06-04 16:45:03 +0545

    bugs correct

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,26 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
 
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import App from "./App"
+import "./index.css"
 
-import App from './App.jsx'
+ReactDOM.createRoot(document.getElementById("root")).render(
 
+  <React.StrictMode>
 
+    <BrowserRouter>
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+      <App />
+
+    </BrowserRouter>
+
+  </React.StrictMode>
+
 )
-
-ReactDOM.createRoot(
-  document.getElementById("root")
-).render(
-
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-
-);
--- server/app/main.py
+++ server/app/main.py
@@ -21,6 +21,7 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://portfolio-apps-kappa.vercel.app",
+    "https://portfolio-apps-aakashsah247s-projects.vercel.app",
 ]
 
 app.add_middleware(
